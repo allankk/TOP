d60b7cdc2e945fb5091e99159b7e9aefa2b5b974
allankk <allankossas@gmail.com>
2020-05-01 18:26:16 +0300

    File restructure

---
+++ WEB DEVELOPMENT 101/JS/test.js
@@ -0,0 +1,28 @@
+// practice 1
+function add7(num1) {
+    return num1 + 7;
+}
+
+//console.log(add7(4));
+
+// practice 2
+let multiply = (num1, num2) => num1 * num2;
+//console.log(multiply(2,3));
+
+// practice 3
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+//console.log(capitalize("Thisplace"));
+//console.log(capitalize("thiSPlAcE"));
+//console.log(capitalize("hellO tHis Is TesTing"));
+
+// practice 4
+
+function lastletter(string) {
+    return string.slice(-1);
+}
+
+//console.log(lastletter("trial"));
+//console.log(lastletter("hellO tHis Is TesTing"));
+
