463879cc2a5ca4fa75713bea5b2229e61aec92b4
allankk <allankossas@gmail.com>
2020-05-01 21:32:50 +0300

    bugfix AI

--- WEB DEVELOPMENT 101/JS/Rock-Paper-Scissors/scripts/play.js
+++ WEB DEVELOPMENT 101/JS/Rock-Paper-Scissors/scripts/play.js
@@ -8,26 +8,25 @@ const DRAW = 'THIS ROUND IS A DRAW';
 
 let playerScore = 0;
 let opponentScore = 0;
+let drawScore = 0;
 let roundsPlayed = 0;
 
 
 //Updates the scores in HTML
 function updateScores(){
-    document.getElementById("playerScore").innerHTML = "PLAYER SCORE: " + playerScore;
-    document.getElementById("opponentScore").innerHTML = "OPPONENT SCORE: " + opponentScore;
+    document.getElementById("playerScore").innerHTML = "PLAYER WINS: " + playerScore;
+    document.getElementById("opponentScore").innerHTML = "OPPONENT WINS: " + opponentScore;
+    document.getElementById("drawScore").innerHTML = "DRAWS: " + drawScore;
 }
 
 //Updates the HTML of last round win.
 function updateRunningInfo(result){
     if (result == PLAYER_WIN) {
-        console.log(PLAYER_WIN)
         document.getElementById("lastRound").innerHTML = PLAYER_WIN;
     }
     else if (result == OPPONENT_WIN) {
-        console.log(OPPONENT_WIN);
         document.getElementById("lastRound").innerHTML = OPPONENT_WIN;
     } else {
-        console.log(DRAW);
         document.getElementById("lastRound").innerHTML = DRAW;
     }
 }
@@ -36,7 +35,6 @@ function updateRunningInfo(result){
 function checkIfGameIsOver(){
     
     if (roundsPlayed >= 5) {
-        console.log(roundsPlayed);
         let element = document.getElementById("lastRound");
 
         if(playerScore > opponentScore) {
@@ -51,13 +49,16 @@ function checkIfGameIsOver(){
 
 //Returns a random play.
 function computerPlay() {
+    
     let possiblePlays = [ROCK, PAPER, SCISSORS];
-   
-    return possiblePlays[Math.floor(Math.random() * possiblePlays.length)];
+    let play = possiblePlays[Math.floor(Math.random() * possiblePlays.length)];
+
+    return play;
 }
 
 //Plays one round of rock-paper-scissors
-function playRound(playerChoice, opponentChoice) {
+function playRound(playerChoice) {
+    let opponentChoice = computerPlay();
 
     if(playerChoice == ROCK) {
         if(opponentChoice == ROCK) message = DRAW;
@@ -77,6 +78,8 @@ function playRound(playerChoice, opponentChoice) {
         playerScore++;
     } else if (message == OPPONENT_WIN) {
         opponentScore++;
+    } else {
+        drawScore++;
     }
 
     roundsPlayed++;
@@ -91,63 +94,9 @@ function loadEventHandlers(){
     let btnScissors = document.getElementById("btn.SCISSORS");
     let btnPaper = document.getElementById("btn.PAPER");
 
-    btnRock.addEventListener("click", playRound.bind(null, ROCK, computerPlay()));
-    btnScissors.addEventListener("click", playRound.bind(null, SCISSORS, computerPlay()));
-    btnPaper.addEventListener("click", playRound.bind(null, PAPER, computerPlay()));
+    btnRock.addEventListener("click", playRound.bind(null, ROCK));
+    btnScissors.addEventListener("click", playRound.bind(null, SCISSORS));
+    btnPaper.addEventListener("click", playRound.bind(null, PAPER));
 }
 
 loadEventHandlers();
-
-/*Tests for one-time game
-const playerChoice = ROCK;
-const opponentChoice = computerPlay();
-console.log(playRound(playerChoice, opponentChoice))
-
-function game() {
-    let playerScore = 0;
-    let opponentScore = 0;
-
-    let btnRock = document.getElementById("btn.ROCK");
-    let btnScissors = document.getElementById("btn.SCISSORS");
-    let btnPaper = document.getElementById("btn.PAPER");
-
-    btnRock.addEventListener("click", playRound(ROCK, computerPlay()));
-    btnScissors.addEventListener("click", playRound(SCISSORS, computerPlay()));
-    btnPaper.addEventListener("click", playRound(PAPER, computerPlay()));
-
-}
-
-// Plays a five-round game
-function gameWithPrompt() {
-    let playerScore = 0;
-    let opponentScore = 0;
-
-    let i;
-    // play every round. Call the winner and the scores each time
-    for (i = 0; i < 5; i++) {
-        let playerChoice = prompt("Player choice (rock/paper/scissors):");
-        let result = playRound(playerChoice, computerPlay());
-
-        if (result == PLAYER_WIN) {
-            playerScore++;
-            console.log(PLAYER_WIN)
-            document.getElementById("lastRound").innerHTML = PLAYER_WIN;
-        }
-        else if (result == OPPONENT_WIN) {
-            opponentScore++;
-            console.log(OPPONENT_WIN);
-            document.getElementById("lastRound").innerHTML = OPPONENT_WIN;
-        } else {
-            console.log(DRAW);
-            document.getElementById("lastRound").innerHTML = DRAW;
-        }
-
-        console.log("Player score: " + playerScore + "\nOpponent Score: "+ opponentScore);
-    }
-
-    if(playerScore > opponentScore) console.log("PLAYER WINS THE GAME OF FIVE WITH " + playerScore + "POINTS TO " + opponentScore + ".");
-    else if (opponentScore > playerScore) console.log("COMPUTER WINS THE GAME OF FIVE WITH " + opponentScore + "POINTS TO " + playerScore + ".");
-    else console.log("THE GAME ENDED IN A DRAW WITH BOTH PLAYERS HAVING " + playerScore + " POINTS.");
-}
-
-*/
