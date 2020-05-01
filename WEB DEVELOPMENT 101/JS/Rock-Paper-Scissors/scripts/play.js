9ba2d5039110c62c96018ded703213f012b7442c
allankk <allankossas@gmail.com>
2020-05-01 20:54:34 +0300

    fixed EventListeners, HTML working

--- WEB DEVELOPMENT 101/JS/Rock-Paper-Scissors/scripts/play.js
+++ WEB DEVELOPMENT 101/JS/Rock-Paper-Scissors/scripts/play.js
@@ -6,11 +6,58 @@ const OPPONENT_WIN = 'COMPUTER WINS THIS ROUND';
 const PLAYER_WIN = 'PLAYER WINS THIS ROUND';
 const DRAW = 'THIS ROUND IS A DRAW';
 
+let playerScore = 0;
+let opponentScore = 0;
+let roundsPlayed = 0;
+
+
+//Updates the scores in HTML
+function updateScores(){
+    document.getElementById("playerScore").innerHTML = "PLAYER SCORE: " + playerScore;
+    document.getElementById("opponentScore").innerHTML = "OPPONENT SCORE: " + opponentScore;
+}
+
+//Updates the HTML of last round win.
+function updateRunningInfo(result){
+    if (result == PLAYER_WIN) {
+        console.log(PLAYER_WIN)
+        document.getElementById("lastRound").innerHTML = PLAYER_WIN;
+    }
+    else if (result == OPPONENT_WIN) {
+        console.log(OPPONENT_WIN);
+        document.getElementById("lastRound").innerHTML = OPPONENT_WIN;
+    } else {
+        console.log(DRAW);
+        document.getElementById("lastRound").innerHTML = DRAW;
+    }
+}
+
+//Checks if the game is over and makes an endgame prompt if so
+function checkIfGameIsOver(){
+    
+    if (roundsPlayed >= 5) {
+        console.log(roundsPlayed);
+        let element = document.getElementById("lastRound");
+
+        if(playerScore > opponentScore) {
+            element.innerHTML = "PLAYER WINS THE GAME OF FIVE WITH " + playerScore + " POINTS TO " + opponentScore + ".";
+        } else if (opponentScore > playerScore) {
+            element.innerHTML = "COMPUTER WINS THE GAME OF FIVE WITH " + opponentScore + " POINTS TO " + playerScore + ".";
+        } else {
+            element.innerHTML = "THE GAME ENDED IN A DRAW WITH BOTH PLAYERS HAVING " + playerScore + " POINTS.";
+        } 
+    }
+}
+
+//Returns a random play.
+function computerPlay() {
+    let possiblePlays = [ROCK, PAPER, SCISSORS];
+   
+    return possiblePlays[Math.floor(Math.random() * possiblePlays.length)];
+}
+
 //Plays one round of rock-paper-scissors
 function playRound(playerChoice, opponentChoice) {
-    let message;
-    playerChoice = playerChoice.toUpperCase();
-
 
     if(playerChoice == ROCK) {
         if(opponentChoice == ROCK) message = DRAW;
@@ -25,25 +72,53 @@ function playRound(playerChoice, opponentChoice) {
         else if (opponentChoice == SCISSORS) message = OPPONENT_WIN;
         else message = DRAW;
     }
+
+    if (message == PLAYER_WIN) {
+        playerScore++;
+    } else if (message == OPPONENT_WIN) {
+        opponentScore++;
+    }
+
+    roundsPlayed++;
     
-    return message;
+    updateRunningInfo(message);
+    updateScores();
+    checkIfGameIsOver();   
 }
 
-//Returns a random play.
-function computerPlay() {
-    let possiblePlays = [ROCK, PAPER, SCISSORS];
-    
-    return possiblePlays[Math.floor(Math.random() * possiblePlays.length)];
+function loadEventHandlers(){
+    let btnRock = document.getElementById("btn.ROCK");
+    let btnScissors = document.getElementById("btn.SCISSORS");
+    let btnPaper = document.getElementById("btn.PAPER");
+
+    btnRock.addEventListener("click", playRound.bind(null, ROCK, computerPlay()));
+    btnScissors.addEventListener("click", playRound.bind(null, SCISSORS, computerPlay()));
+    btnPaper.addEventListener("click", playRound.bind(null, PAPER, computerPlay()));
 }
+
+loadEventHandlers();
 
 /*Tests for one-time game
 const playerChoice = ROCK;
 const opponentChoice = computerPlay();
 console.log(playRound(playerChoice, opponentChoice))
-*/
+
+function game() {
+    let playerScore = 0;
+    let opponentScore = 0;
+
+    let btnRock = document.getElementById("btn.ROCK");
+    let btnScissors = document.getElementById("btn.SCISSORS");
+    let btnPaper = document.getElementById("btn.PAPER");
+
+    btnRock.addEventListener("click", playRound(ROCK, computerPlay()));
+    btnScissors.addEventListener("click", playRound(SCISSORS, computerPlay()));
+    btnPaper.addEventListener("click", playRound(PAPER, computerPlay()));
+
+}
 
 // Plays a five-round game
-function game() {
+function gameWithPrompt() {
     let playerScore = 0;
     let opponentScore = 0;
 
@@ -75,5 +150,4 @@ function game() {
     else console.log("THE GAME ENDED IN A DRAW WITH BOTH PLAYERS HAVING " + playerScore + " POINTS.");
 }
 
-
-
+*/
