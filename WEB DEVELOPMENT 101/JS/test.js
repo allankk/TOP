3a0b17d6cc7df931eb4950cfe0985efbf4e57edf
allankk <allankossas@gmail.com>
2020-05-07 12:54:44 +0300

    completed DOM exercises

--- WEB DEVELOPMENT 101/JS/test.js
+++ WEB DEVELOPMENT 101/JS/test.js
@@ -1,28 +1,45 @@
-// practice 1
-function add7(num1) {
-    return num1 + 7;
+
+function createParagraph(color) {
+    const div = document.createElement('div');
+    div.style.color = color;
+    div.textContent = "Hey I'm " + color + "!";
+    div.classList = "addedDiv"
+
+    const container = document.querySelector(".test");
+    container.appendChild(div);
 }
 
-//console.log(add7(4));
+function createH3(color) {
+    const h3 = document.createElement("h3");
+    h3.style.color = color;
+    h3.textContent = "I'm a " + color + " h3!";
+    h3.classList = "addedH3";
 
-// practice 2
-let multiply = (num1, num2) => num1 * num2;
-//console.log(multiply(2,3));
-
-// practice 3
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-}
-//console.log(capitalize("Thisplace"));
-//console.log(capitalize("thiSPlAcE"));
-//console.log(capitalize("hellO tHis Is TesTing"));
-
-// practice 4
-
-function lastletter(string) {
-    return string.slice(-1);
+    const container = document.querySelector(".test");
+    container.appendChild(h3);
 }
 
-//console.log(lastletter("trial"));
-//console.log(lastletter("hellO tHis Is TesTing"));
+function createNewDiv(borderColor, backgroundColor) {
+    const addedDiv = document.createElement("div");
+    addedDiv.classList = "addedDiv";
+    addedDiv.style.border = "1px solid " + borderColor;
+    addedDiv.style.backgroundColor = backgroundColor;
+    
+    const h1 = document.createElement("h1");
+    h1.classList = "addedH1";
+    h1.textContent = "I'm in a div";
+    
+    const addedParagraph = document.createElement("p");
+    addedParagraph.textContent = "ME TOO!";
+     
+    addedDiv.appendChild(h1);
+    addedDiv.appendChild(addedParagraph);
 
+    const container = document.querySelector("body");
+    const addBefore = document.querySelector(".test");
+    container.insertBefore(addedDiv, addBefore);
+}
+
+createH3("blue");
+createParagraph("red");
+createNewDiv("black", "pink");
