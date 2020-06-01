ee9bc2b3e108ea3a65a795505f2c5aef3293bab8
allankk <allankossas@gmail.com>
2020-06-01 22:30:36 +0300

    tic-tac-toe functional part completed

--- WEB DEVELOPMENT 101/JS/Tic-Tac-Toe/tic-tac-toe.js
+++ WEB DEVELOPMENT 101/JS/Tic-Tac-Toe/tic-tac-toe.js
@@ -1,4 +1,7 @@
-//  TODO: BUG when restarting the game. Top right buttons don't display anything (value is still counted)
+const WIN = 'win';
+const DRAW = 'draw';
+const NOTOVER = 'not-over';
+
 
 let player1;
 let player2;
@@ -10,6 +13,7 @@ const gameBoard = (() => {
 
     const checkBoard = (row, column, value) => {
         let checkResults = [true, true, true, true]; // checkresults - if not equal to the mark, will change to false.
+        let checkDraw = true;
 
         for (let i = 0; i <= 2; i++) {  // check the same row and column of inserted value
             if (getValue(row, i) != value) checkResults[0] = false;
@@ -24,11 +28,20 @@ const gameBoard = (() => {
             if (getValue(grid[0], grid[1]) != value) checkResults[3] = false;
         });
 
-        return (checkResults.includes(true)) ? true : false; // if any of the checking tests succeeded, return true (game won)
+        // check for a draw
+        board.forEach(square => {
+            if (square.includes('')) checkDraw = false;  
+        })
+
+        // RETURN WIN, DRAW or NOTOVER
+        if (checkDraw == true) return DRAW
+
+        return (checkResults.includes(true)) ? WIN : NOTOVER; // if any of the checking tests succeeded, return true (game won)
+
     };
 
     const clearBoard = () => {
-        obj.board = [['', '', ''], ['', '', ''], ['', '', '']];
+        board = [['', '', ''], ['', '', ''], ['', '', '']];
     };
 
     const getValue = (row, column) => board[row][column]; 
@@ -48,17 +61,34 @@ const gameBoard = (() => {
 
 const Player = (name, mark) => { // mark defines the symbol used, i.e. 'X' or 'O'
     let turn = false;
+
+    const checkResults = (row, column, mark) => {
+        const checkResult = gameBoard.checkBoard(row, column, mark);
+        
+        if (checkResult == WIN) {
+            console.log(`${name} has won!`);
+            displayController.updateInfo(name, WIN);
+            displayController.toggleBoard('disable');
+            gridEvents.removeGridEvents();
+        } else if (checkResult == DRAW) {
+            displayController.updateInfo(name, DRAW);
+            console.log('Game ended in a draw');
+        } else {
+            console.log(`player1 turn: ${player1.turn}`);
+            console.log(`player2 turn: ${player2.turn}`);
+
+            displayController.updateInfo(name, NOTOVER)
+        }
+    }
     
     const play = (row, column) => {
         if (gameBoard.setValue(row, column, mark)) {
             displayController.updateBoard(row, column, mark);
+            checkResults(row, column, mark);
             toggleTurn();
-        }
-            
-        if (gameBoard.checkBoard(row, column, mark)) {
-            console.log(`${name} has won!`);
-            displayController.toggleBoard('disable');
-            gridEvents.removeGridEvents();
+
+
+            // displayController.updateInfo();
         }
     }
 
@@ -73,6 +103,9 @@ const Player = (name, mark) => { // mark defines the symbol used, i.e. 'X' or 'O
 const displayController = (() => {
     const gameBoardItem = document.querySelector('.game-board'); // get the game-board section from DOM
     const squareItems = document.querySelectorAll('.square');
+    const startBtn = document.querySelector('#start-game');
+    const gameInfo = document.querySelector('.game-info');
+    const infoBoard = document.getElementById('info-board');
 
     const clearBoard = () => {
         gameBoard.clearBoard();
@@ -94,6 +127,24 @@ const displayController = (() => {
         let squareItem = document.getElementById(`square-${numOfSquare}`);
         squareItem.classList.toggle('disabled');
         squareItem.innerHTML = mark;
+    }
+
+    const displayInfo = () => {
+        gameInfo.classList.add('display-flex');
+    }
+
+    const updateInfo = (playername, action) => { // Action either WIN, DRAW or NOTOVER
+        if (action == WIN) {
+            infoBoard.textContent = `${playername} HAS WON`;
+        } else if (action == DRAW) {
+            infoBoard.textContent = 'DRAW'
+        } else {
+            if (player1.turn == true) {
+                infoBoard.textContent = `${player2.name}'s turn`;
+            } else {
+                infoBoard.textContent = `${player1.name}'s turn`;
+            };
+        };
     }
 
     const translateCoordinatesToSquare = (row, column) => { // translates grid number to the number of the square
@@ -118,7 +169,8 @@ const displayController = (() => {
         }
     };
 
-    return {updateBoard, translateSquareToCoordinates, toggleBoard, clearBoard};
+
+    return {updateBoard, translateSquareToCoordinates, toggleBoard, clearBoard, displayInfo, updateInfo};
 })();
 
 
@@ -151,22 +203,55 @@ const gridEvents = (() => {
 
 function addButtonEvents() {
     const startBtn = document.getElementById('start-game');
-    startBtn.addEventListener('click', startGame);
+    startBtn.addEventListener('click', function() {
+        if (startBtn.textContent == 'RESTART') {
+            restartGame();
+        } else {
+            startBtn.textContent = 'RESTART';
+            startGame();
+        }
+    });
 }
 
-// TODO: clear the board when game starts or restarts
+function restartGame() {
+    displayController.clearBoard();
+    gridEvents.addGridEvents();
+    displayController.toggleBoard('enable');
+    player1.turn = false; // first the turns are reversed for updateInfo to update DOM correctly.
+    player2.turn = true;
+    displayController.updateInfo();
+    player1.turn = true; // Enables the turn of player1 when game starts
+    player2.turn = false;
+}
 
 function startGame() {
     displayController.clearBoard();
     gridEvents.addGridEvents();
-
     displayController.toggleBoard('enable');
-    player1 = Player('Player 1', 'O');
-    player2 = Player('Player 2', 'X');
-    player1.toggleTurn(); // Enables the turn of player1 when game starts
 
-    // player1.play(1, 1);
-    // player2.play(0, 2);
+    const inputPlayer1 = document.getElementById('player1-name');
+    const inputPlayer2 = document.getElementById('player2-name');
+
+    if (inputPlayer1.value == "") {
+        player1 = Player('Player 1', 'X');
+    } else {
+        player1 = Player(inputPlayer1.value, 'X');
+    }
+
+    if (inputPlayer2.value == "") {
+        player2 = Player('Player 2', 'O');
+    } else {
+        player2 = Player(inputPlayer2.value, 'O');
+    }
+
+    document.querySelector('.player-info').classList.add('display-none'); // Hide the player inputs
+
+    player1.turn = false; // first the turns are reversed for updateInfo to update DOM correctly.
+    player2.turn = true;
+    displayController.displayInfo();
+    displayController.updateInfo();
+    player1.turn = true; // Enables the turn of player1 when game starts
+    player2.turn = false;
 }
 
 displayController.toggleBoard('disable');
