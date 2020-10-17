a16612f4f58717ddbcf848a113489e0106578724
allankk <allankossas@gmail.com>
2020-10-17 14:49:25 +0300

    restaurant page started

---
+++ Restaurant-Page/src/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap';
+
+console.log("hello again");
+
+// function App() {
+//     const navbar = document.createElement('div');
+
+// }
+
+// const body = document.getElementById('content');
+// document.appendChild(App());
