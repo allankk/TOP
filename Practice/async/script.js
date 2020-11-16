4ba9774c3c0e355ee794460ae0eb7438f1789f25
allankk <allankossas@gmail.com>
2020-11-16 21:10:16 +0200

    async practice

---
+++ Practice/async/script.js
@@ -0,0 +1,50 @@
+// openweathermap api key: ccc5bdcd6b167bc37faefb623abeb33b
+// api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ccc5bdcd6b167bc37faefb623abeb33b
+
+// giphy api key: 8KEGVlMVUDkRJo0m45A49fuBY8wCHZI4
+// https://api.giphy.com/v1/gifs/translate?api_key=8KEGVlMVUDkRJo0m45A49fuBY8wCHZI4&s=hello
+
+const searchGIF = (searchTerm) => {
+    clearErrors();
+
+    const url = 'https://api.giphy.com/v1/gifs/translate?api_key=8KEGVlMVUDkRJo0m45A49fuBY8wCHZI4&s='
+    const input = document.getElementById('search');
+
+    if (searchTerm) {
+        getImage(`${url} + ${searchTerm}`);
+    } else {
+        getImage(`${url} + ${input.value}`)
+    }
+}
+
+const getImage = (url) => {
+    const img = document.querySelector('img');
+
+    fetch(url, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+        })
+        .catch((error) => {
+            console.log(error);
+
+            const text = document.createElement('h2');
+            text.innerHTML = "Could not find image or connect to API";
+
+            img.parentNode.appendChild(text);
+        })
+};
+
+const clearErrors = () => {
+    const error = document.querySelector('h2');
+
+    if (error != undefined || error != null) {
+        error.parentNode.removeChild(error);
+    }
+}
+
+searchGIF('hello');
+
+
