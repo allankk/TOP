814ace62f38f61e3fc819750177a42430872ee67
allankk <allankossas@gmail.com>
2021-01-09 18:50:42 +0200

    express_local_library added routes

---
+++ Practice/express_local_library/controllers/bookinstanceController.js
@@ -0,0 +1,41 @@
+var BookInstance = require('../models/bookinstance');
+
+// Display list of all BookInstances
+exports.bookinstance_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance list')
+};
+
+// Display detail page for a specific bookinstance
+exports.bookinstance_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Bookinstance detail: ' + req.params.id);
+};
+
+// Display BookInstance Create form on GET
+exports.bookinstance_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: BookInstance create GET');
+};
+
+// Handle Bookinstance create on POST
+exports.bookinstance_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Bookinstance create POST');
+};
+
+// Display Bookinstance delete form on GET
+exports.bookinstance_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Bookinstance create GET');
+};
+
+// Display BookInstance delete on POST
+exports.bookinstance_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Bookinstance delete POST');
+};
+
+// Display Bookinstance update form on GET
+exports.bookinstance_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Bookinstance update GET');
+};
+
+// Display BookInstance update on POST
+exports.bookinstance_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Bookinstance update POST');
+};
